cb00437bb3778da36896b80e96711c2fd4805a59
Gia Phong <ngphong.7800@gmail.com>
2023-11-21 09:35:50 +0700

    update navbar & sidebar and reponsive

--- src/component/Home/Navbar/navbar.tsx
+++ src/component/Home/Navbar/navbar.tsx
@@ -6,9 +6,9 @@ import { MenuOutlined } from "@ant-design/icons";
 function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  // const toggleMobileMenu = () => {
-  //   setIsMobileMenuOpen(!isMobileMenuOpen);
-  // };
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <>
@@ -19,6 +19,12 @@ function Navbar() {
           <OrtherOfNavbar />
         </div>
       </div>
+      <button
+        onClick={toggleMobileMenu}
+        className="lg:hidden h-5 bg-slate-500"
+      >
+         <MenuOutlined />
+      </button>
       {isMobileMenuOpen && (
         <div className="flex w-[242px] h-screen p-[20px_0px_272px_0px] flex-col gap-39 text-center bg-white flex-shrink-0">
           <div>
